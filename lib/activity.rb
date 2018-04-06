5813481aa05de4d7ade7a6c7596826d43b525c60
Seth Schwartz <sschwartz214@gmail.com>
2018-04-05 18:09:47 -0600

    first commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Activity
+  attr_reader :type,
+              :participants
+
+  def initialize(type)
+    @type = type
+    @participants = {}
+    @total_cost = 0
+
+  end
+
+  # binding.pry
+  def add_participant(name, cost)
+    @participants[name] = cost
+  end
+
+  def total_cost
+    @participants.values.sum
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,35 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_has_attributes
+    activity = Activity.new("hiking")
+
+    assert_equal "hiking", activity.type
+    assert_equal ({}), activity.participants
+  end
+
+  def test_add_participant
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Chad", 10)
+    activity.add_participant("Brad", 10)
+    expected = {"Chad" => 10,
+                "Brad" => 10
+                }
+
+
+    assert_equal expected, activity.participants
+  end
+
+  def test_total_cost
+    activity = Activity.new("hiking")
+
+    activity.add_participant("Chad", 10)
+    activity.add_participant("Brad", 10)
+
+    assert_equal 20, activity.total_cost
+
+  end
+
+
 end
